1a678f27a7f9fbd0c8d5f0645c9b1555661c0997
Federico Sirio <federico.sirio@globant.com>
2024-09-07 01:26:51 -0300

    asd

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket = "terraform-state-bucket-latam_test"
     prefix = "terraform/state"
-    credentials = "/home/federico/repos/me/latam-test-434700-38550ce792bf.json"
+    # credentials = "/home/federico/repos/me/latam-test-434700-38550ce792bf.json"
   }
 }
